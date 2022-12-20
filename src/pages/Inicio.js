f50a9cdb4b3fa1d0c759ea3fc9cdb03235b5af9e
Sharon Arana <shar_mdza@hotmail.com>
2022-12-19 19:04:09 -0600

    Añadiendo primer template y primer página

---
+++ src/pages/Inicio.js
@@ -0,0 +1,20 @@
+const Inicio = () => {
+    const view = `
+        <section class="inicio">
+        <figure class="inicio-logo">
+            <img src="" alt="imagen de logo: tindog"/>
+        </figure>
+        <figure class="inicio-bienvenida">
+            <img src="" alt="imagen de bienvenida"/>
+        </figure>
+        <div class="inicio-botones">
+            <button class="registro-btn">Registrate</button>
+            <span>|</span>
+            <button class="iniciarSesion-btn">Inicia sesión</button>
+        </div>
+        </section>
+    `;
+    return view;
+}
+
+export default Inicio;
