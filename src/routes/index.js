a4b252e887f17fe9aca435a31831b14449b5f40c
Sharon Arana <shar_mdza@hotmail.com>
2022-12-20 10:31:54 -0600

    Añadiendo rutas del proyecto

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,16 @@ import Error404 from '../pages/Error404';
 const routes = {
     '/': Inicio,
     '/404': Error404,
+    '/inicio-sesion': 'InicioSesion',
+    '/registro-usuario': 'RegistroUsuario',
+    '/formulario-registro': 'FormularioRegistro',
+    '/home': 'Home',
+    '/match': 'Match',
     '/:id': 'PerfilUsuario',
+    '/match/:id': 'PerfilMatch',
+    '/busqueda-usuario': 'BusquedaUsuario',
 };
+
+const router = async () => {
+
+}
