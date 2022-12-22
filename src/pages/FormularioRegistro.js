dae694d293d7e6522aa440fd91990900ce4f408a
prisalvarado18 <Priscila.Alvarado@studentambassadors.com>
2022-12-22 11:13:12 -0500

    Añadí la estructura del registro de usuario

---
+++ src/pages/FormularioRegistro.js
@@ -0,0 +1,10 @@
+const FormularioRegistro = () => {
+    const view = `
+        <section class="registro-usuario">
+            <h3>Formulario de usuario</h3>
+        </section>
+    `;
+    return view;
+};
+
+export default FormularioRegistro;
