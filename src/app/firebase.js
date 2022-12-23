35719fd2b3f0e0fbee23dc5bf1e5f39e4b425a01
vickymendoza <vickymendoza90@gmail.com>
2022-12-22 21:03:38 -0500

    aun no toma cambios

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -4,21 +4,19 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-//import firebase from "firebase/app";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
 
 // configuración de la app de firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCVFwSqmwf3nPLqyBd-_SrnWdKfmv8kRVc",
-    authDomain: "petblr-74086.firebaseapp.com",
-    projectId: "petblr-74086",
-    storageBucket: "petblr-74086.appspot.com",
-    messagingSenderId: "431725901053",
-    appId: "1:431725901053:web:0804da9ec879761fd6ba94"
+apiKey: "AIzaSyCVFwSqmwf3nPLqyBd-_SrnWdKfmv8kRVc",
+authDomain: "petblr-74086.firebaseapp.com",
+projectId: "petblr-74086",
+storageBucket: "petblr-74086.appspot.com",
+messagingSenderId: "431725901053",
+appId: "1:431725901053:web:0804da9ec879761fd6ba94"
 };
 
 // inicialización de firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 // uso de firebase auth
-export const firebaseAuth = getAuth(firebaseApp);
-
+export const firebaseAuth = getAuth(firebaseApp)
--- src/app/register.js
+++ src/app/register.js
@@ -1,19 +1,15 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
 import { firebaseAuth } from "./firebase.js"
 
-const registerForm = document.getElementById("registerForm");
+const registerForm = document.getElementById("registerForm")
 
 // async indica que la función es asincrona, esto también se puede hacer con .then(), pero async/await es más moderno
 // las funciones asincronas SIEMPRE dan promesas, que sería lo que va con await o .then()
 registerForm.addEventListener("submit", async (e)=>{
     // evita que se actialice la pag una vez se presiona "enter" o REGISTRARSE
     e.preventDefault()
-    //const email = registerForm['registerDivPetEmailInput'].value;
-    const email = registerForm['registerFormPetEmailInput'].value;
-                                
-    //const psw = registerForm['registerDivPasswordInput'].value;
-    const psw = registerForm['registerFormPasswordInput'].value;
-
+    const email = registerForm['registerDivPetEmailInput'].value
+    const psw = registerForm['registerDivPasswordInput'].value
     // try{} catch (error){} es como el .then() y .catch() respectivamente
     // catch (error) solo corre si la función falla
     // await SIEMPRE se usa con async, de lo contrario tira error.
@@ -23,11 +19,11 @@ registerForm.addEventListener("submit", async (e)=>{
         // 2. correo (email)
         // 3. contraseña (psw)
     try{
-        const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, psw);
-        document.getElementById("authFunciona").classList.remove("hidden");
-        console.log(userCredential);
+        const userCredential = await createUserWithEmailAndPassword(firebaseAuth, email, psw)
+        document.getElementById("authFunciona").classList.remove("hidden")
+        console.log(userCredential)
     } catch (error) {
-        document.getElementById("authError").classList.remove("hidden");
-        console.log(error);
+        document.getElementById("authError").classList.remove("hidden")
+        console.log(error)
     }
 })
