e1b826771e6d22d3e1c58ef70dbe9f47815d6f80
Nicole Henríquez <ale.nicolehs@gmail.com>
2022-12-23 16:23:41 -0300

    creación de modal sign up, se crea style.css

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const openModalSU = () => {
+    modalSU.style.display = 'flex';
+  }
+
+const closeModalSU = () => {
+    modalSU.style.display = 'none';
+  }
+  
+const btnOpenModalSU = document.getElementById('botonRegistrar');
+btnOpenModalSU.addEventListener('click', openModalSU);
+
+const btnCloseModalSU = document.getElementById('botonCerrarModal');
+btnCloseModalSU.addEventListener('click', closeModalSU);
+
+
