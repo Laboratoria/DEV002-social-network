e1d313813c675bcfd34e81b55a420e60fa69c6af
Laura Buitrago <buitrago92943@gmail.com>
2022-12-27 13:02:27 -0500

    Incorporacion SPA login

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLeEKdWYHeyxAAPoZ3-tpRs85uDEc9iYg",
   authDomain: "reading-club-43baf.firebaseapp.com",
-  databaseURL: "https://reading-club-43baf-default-rtdb.firebaseio.com",
   projectId: "reading-club-43baf",
   storageBucket: "reading-club-43baf.appspot.com",
   messagingSenderId: "93329318817",
   appId: "1:93329318817:web:b0b22eb377191fa03be1e0",
-  measurementId: "G-QSKJRLQ5Z8"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+ export const firebaseApp = initializeApp(firebaseConfig);
+ export const firebaseAuth = getAuth(firebaseApp);
+
 
---
+++ src/lib/view/login.js
@@ -0,0 +1,3 @@
+import { createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
+import { firebaseAuth } from "../firebase.js"
+
