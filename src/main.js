febfa710caea3e19a7707c641d1954a390bcb0ea
sanlorenita-18 <sanlorenita-18@hotmail.com>
2022-12-28 15:42:11 -0500

    se agrega la ruta home en main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,21 @@ import { myFunction } from './lib/index.js';
 import {login} from './login.js';
 import {home} from './home.js'
 
+
 login();
 console.log(home());
 
 
+const route = {
+    "/" :  home,
+    "/login": login
+}
 
 window.addEventListener('DOMContentLoaded',() =>{
     const main = document.getElementById('containerMain')
     main.appendChild(home())
     main.classList.add('containerMain')
+    document.replaceChild('home()')
 })
 
 
