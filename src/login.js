bd57d1c3ffedf6d7fbd31501c683b85e4b0cf15f
sanlorenita-18 <sanlorenita-18@hotmail.com>
2022-12-29 15:41:50 -0500

    se implementó la primera interfaz de la aplicación y se le añadieron atributos a los formularios

--- src/login.js
+++ src/login.js
@@ -1,3 +1,5 @@
+import { HelpFormatter } from "argparse";
+
   export const login = () => {
     const $section = document.createElement("section"),
   $divLogo = document.createElement("div"),
@@ -9,26 +11,61 @@
   $inputPassword = document.createElement("input"),
   $divContainerCat = document.createElement("div"),
   $imgCat = document.createElement("img"),
-  $inputSingUp = document.createElement("input"),
+  $inputSignUpCat = document.createElement("input"),
   $divContainerSpan = document.createElement("div"),
   $spanOption = document.createElement("span"),
   $divContainerRegister = document.createElement("div"),
   $inputBtnGoogle = document.createElement("input"),
-  $pSingUp = document.createElement("p"),
-  $aSingUp = document.createElement("a");
+  $pSignUp = document.createElement("p"),
+  $inputSignUp = document.createElement("input");
 
   $section.appendChild($divLogo).appendChild($imgLogo)
   $section.appendChild($divContainerForm).appendChild($divForm)
   $divForm.appendChild($divInput).appendChild($inputEmail)
   $divForm.appendChild($divInput).appendChild($inputPassword)
   $divForm.appendChild($divContainerCat).appendChild($imgCat)
-  $divForm.appendChild($divContainerCat).appendChild($inputSingUp)
+  $divForm.appendChild($divContainerCat).appendChild($inputSignUpCat)
   $divForm.appendChild($divContainerSpan).appendChild($spanOption)
   $divForm.appendChild($divContainerRegister).appendChild($inputBtnGoogle)
-  $divForm.appendChild($divContainerRegister).appendChild($pSingUp).appendChild($aSingUp)
+  $divForm.appendChild($divContainerRegister).appendChild($pSignUp).appendChild($inputSignUp)
+//agregamo atributos
+$divForm.setAttribute('id','formLogin')
+$divForm.setAttribute('class','formLogin')
+
+$inputEmail.setAttribute('type','email')
+$inputEmail.setAttribute('class','emailUser')
+$inputEmail.setAttribute('id','emailUserId')
+$inputEmail.setAttribute('placeHolder','Username')
+
+$inputPassword.setAttribute('type','password')
+$inputPassword.setAttribute('class','passwordUser')
+$inputPassword.setAttribute('id','passwordUserId')
+$inputPassword.setAttribute('placeHolder','Password')
+
+$imgCat.setAttribute('src','img/gato.png')
+$imgCat.setAttribute('alt','gato negro decorativo')
+
+$inputSignUpCat.setAttribute('type','submit')
+$inputSignUpCat.setAttribute('name','btn_signUpCat')
+$inputSignUpCat.setAttribute('class','btn_signUpCat')
+$inputSignUpCat.setAttribute('id','btn_signUpIdCat')
+
+$inputBtnGoogle.setAttribute('type','button')
+$inputBtnGoogle.setAttribute('class','btn_google')
+$inputBtnGoogle.setAttribute('id','btn_googleId')
+$inputBtnGoogle.setAttribute('value','WITH GOOGLE')
+
+$inputSignUp.setAttribute('type','button')
+$inputSignUp.setAttribute('class','btn_signUp')
+$inputSignUp.setAttribute('id','btn_signUpId')
+$inputSignUp.setAttribute('value','SIGN UP')
 
 
-  console.log($section);
+  return $section;
   };
 
   
+
+
+
+ 
