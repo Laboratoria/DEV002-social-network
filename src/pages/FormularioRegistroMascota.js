d6326b7764fb4993912e5f6d1d9945cfa183ecaa
Sharon Arana <shar_mdza@hotmail.com>
2022-12-29 14:59:49 -0600

    Creacion de contenedor: formulario de registro + estilos

--- src/pages/FormularioRegistroMascota.js
+++ src/pages/FormularioRegistroMascota.js
@@ -2,7 +2,19 @@ const FormularioRegistroMascota = () => {
     const contenedor = document.createElement('section');
     contenedor.classList.add('formularioMascota');
     const view = `
-        <p>Aqui van los datos de tu perrito</p>
+        <figure class='logo'>
+            <img src="./assets/tindog_logo_r.png" alt="logo de tindog">
+        </figure>
+        <div class='fotoPerfil'>
+            <figure>
+                <img src="./assets/dog-iconuser.png" alt="imagen de usuario"/>
+            </figure>
+            <button>+</button>
+        </div>
+        <form>
+            <p>Nombre de tu mascota</p>
+            <input type="text" name="" id="" placeholder="Sulivan">
+        </form>
     `;
     contenedor.innerHTML = view;
     return contenedor;
