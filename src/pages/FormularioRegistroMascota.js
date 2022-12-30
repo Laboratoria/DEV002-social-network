7da3ca713a97e66c4e482522591cc7d37a20cfad
Sharon Arana <shar_mdza@hotmail.com>
2022-12-30 16:20:37 -0600

    Añadiendo estilos al formulario de registro parte 2

--- src/pages/FormularioRegistroMascota.js
+++ src/pages/FormularioRegistroMascota.js
@@ -16,14 +16,24 @@ const FormularioRegistroMascota = () => {
                 <label>Nombre de tu mascota</label>
                 <input type="text" id="" placeholder="Sulivan">
             </div>
-            <div>
-                <label>Edad</label>
-                <input type="text" id="" placeholder="8">
-                <label>Sexo</label>
-                <label for="macho">Macho</label>
-                <input type="radio" name="dogSex" value="macho" id="macho">
-                <label for="hembra">Hembra</label>
-                <input type="radio" name="dogSex" value="hembra" id="hembra">
+            <div class='formP2'>
+                <div class='formEdad'>
+                    <label>Edad</label>
+                    <input type="text" id="" placeholder="8">
+                </div>
+                <div class='formDogSex'>
+                    <label>Sexo</label>
+                    <div class='sexOptions'>
+                        <div>
+                            <label for="macho">Macho</label>
+                            <input type="radio" name="dogSex" value="macho" id="macho">
+                        </div>
+                        <div>
+                            <label for="hembra">Hembra</label>
+                            <input type="radio" name="dogSex" value="hembra" id="hembra">
+                        </div>
+                    </div>
+                </div>
             </div>
             <div>
                 <label>Ubicación</label>
@@ -33,20 +43,32 @@ const FormularioRegistroMascota = () => {
                 <label>Raza</label>
                 <input type="text" id="dogRace" placeholder="Chihuahua">
             </div>
-            <div>
+            <div class='formP3'>
                 <label>Tamaño | Talla</label>
-                <label for="pequeña">Pequeña</label>
-                <input type="radio" name="dogSize" value="pequeña" id="pequeña">
-                <label for="mediana">Mediana</label>
-                <input type="radio" name="dogSize" value="mediana" id="mediana">
-                <label for="grande">Grande</label>
-                <input type="radio" name="dogSize" value="grande" id="grande">
+                <div class='tallaOps'>
+                    <div>
+                        <label for="pequeña">Pequeña</label>
+                        <input type="radio" name="dogSize" value="pequeña" id="pequeña">
+                    </div>
+                    <div>
+                        <label for="mediana">Mediana</label>
+                        <input type="radio" name="dogSize" value="mediana" id="mediana">
+                    </div>
+                    <div>
+                        <label for="grande">Grande</label>
+                        <input type="radio" name="dogSize" value="grande" id="grande">
+                    </div>
+                </div>
             </div>
-            <div>
-                <input type="checkbox" id="esterilizacion" name="esterilizacion" value="esterilizacion">
-                <label for="esterilizacion">Esterilización</label>
-                <input type="checkbox" id="vacunas" name="vacunas" value="vacunas">
-                <label for="vacunas">Vacunas al día</label>
+            <div class='formP4'>
+                <div>
+                    <input type="checkbox" id="esterilizacion" name="esterilizacion" value="esterilizacion">
+                    <label for="esterilizacion">Esterilización</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="vacunas" name="vacunas" value="vacunas">
+                    <label for="vacunas">Vacunas al día</label>
+                </div>
             </div>
         </form>
         <div class="botonGuardar">
