e82472ff1c50f4dbc7ac0256bfdad0de914b74a3
filletournesols <dgalarcegonzalez@gmail.com>
2022-12-30 13:30:48 -0300

    arreglo estilos y router con su respectivo template

--- src/js/routes.js
+++ src/js/routes.js
@@ -9,7 +9,6 @@ const PATHS = {
                         Hemos pensado para tí y tu amo esta novedosa red social dónde la
                         estrella serás tú y solamente tú.
                     </a>
-                    <a class="home-welcome">BIENVENID@ A PETBLR</a>
                     <a class="home-question">¿Qué quieres hacer hoy?</a>
                     <button class="home-create-account" id="navLinkCreateAccount">
                         Registrarse
@@ -26,17 +25,12 @@ const PATHS = {
         template: `
             <section class="register" id="register">
                 <form class="register-form" id="registerForm">
-                    <label class="register-form-pet-name" id="registerFormPetName">NOMBRE MASCOTA</label>
                     <input type="name" class="register-form-pet-name-input" id="registerFormPetNameInput" placeholder="Rudolph" required />
-                    <label for="email" class="register-form-pet-email">CORREO:</label>
                     <input type="email" class="register-form-pet-email-input" id="registerFormPetEmailInput" placeholder="hola@petblr.com" required />
-                    <label class="register-form-user-name" id="registerFormUserName">USUARIO</label>
                     <input type="name" class="register-form-user-name-input" id="registerFormUserNameInput" placeholder="Roberto" required />
-                    <label for="password" class="register-form-password">CONTRASEÑA:</label>
-                    <input type="password" class="register-form-password-input" id="registerFormPasswordInput" placeholder="********" required />
+                    <input type="password" class="register-form-password-input" id="registerFormPasswordInput" placeholder="CONTRASEÑA" required />
                     <label class="register-form-psw-recomendation" id="registerFormPswRecomendation">DEBE TENER MÍNIMO 6 CARÁCTERES, UNA MAYÚSCULA Y UN NÚMERO</label>
-                    <label for="password" class="register-form-repeat-password">REPETIR CONTRASEÑA:</label>
-                    <input type="password" class="register-form-repeat-password-input" id="registerFormRepeatPasswordInput"placeholder="********" required />
+                    <input type="password" class="register-form-repeat-password-input" id="registerFormRepeatPasswordInput"placeholder="REPETIR CONTRASEÑA" required />
                     <label class="register-form-repeat-psw hidden" id="registerFormRepeatPsw"> LAS CONTRASEÑAS DEBEN COINCIDIR</label>
                     <button class="register-div-btn" id="registerDivBtn">REGISTRARSE</button>
                 </form>
