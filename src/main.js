7c60b20e96907813f85896eb9dc687728dff7909
Daniela Castro <dgonzalezcs@outlook.com>
2023-01-03 08:09:08 -0600

    interaccion en js de boton registrar

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const indexRegistro = document.getElementById('containerRegister');
+const indexInicioSesion = document.getElementById('container');
+const btnRegistrate = document.getElementById('registrate')
+
+btnRegistrate.addEventListener('click', () => {
+  console.log("click")
+  indexInicioSesion.style.display = "none";
+  indexRegistro.style.display = "block";
+});
+
+function registrar () {
+    let btnIngresar = document.getElementById('ingresar')
+    let email = document.getElementById('email').value;
+    let contrasena = document.getElementById('password').value;
+
+    btnIngresar.addEventListener('click', () => {
+        console.log(email)
+        console.log(contrasena)
+    });
+}
+
+window.addEventListener('load', registrar);
