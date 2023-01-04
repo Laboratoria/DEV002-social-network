18e615329464208cf52b6cafc86717a16739249f
maryyfry <imarione.tt@gmail.com>
2023-01-04 11:01:30 -0600

    merge que tiene cambios de firebase y btn google

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// Importa la biblioteca de Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
+import { getAuth, createUserWithEmailAndPassword} from  'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAHfvbH9LChUrOwAbR4cydwCsmHa7Q330',
+  authDomain: 'usuarias-journey-mates.firebaseapp.com',
+  projectId: 'usuarias-journey-mates',
+  storageBucket: 'usuarias-journey-mates.appspot.com',
+  messagingSenderId: '15257223280',
+  appId: '1:15257223280:web:eecc0cb646124a2f42b4b5',
+  measurementId: 'G-4W8ETMYH7S',
+};
+// Inicializa la aplicación de Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+// Crea una función para registrar usuarios
+export function registerUser(email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      // El usuario ha sido registrado correctamente
+      console.log('Usuario registrado correctamente');
+    })
+    .catch((error) => {
+      // Ocurrió un error al registrar el usuario
+      // Puedes obtener más información sobre el error con error.code y error.message
+      console.error(error);
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,28 +1,18 @@
-// Este es el punto de entrada de tu aplicacion
-
-// import { myFunction } from './lib/index.js';
-
-// jjno myFunction();
-
+import { registerUser } from './index.js';
+// myFunction();
 const indexRegistro = document.getElementById('containerRegister');
 const indexInicioSesion = document.getElementById('container');
 const btnRegistrate = document.getElementById('registrate')
-
 btnRegistrate.addEventListener('click', () => {
-  console.log("click")
   indexInicioSesion.style.display = "none";
   indexRegistro.style.display = "block";
 });
-
-function registrar () {
-    let btnIngresar = document.getElementById('ingresar')
-    let email = document.getElementById('email').value;
-    let contrasena = document.getElementById('password').value;
-
-    btnIngresar.addEventListener('click', () => {
-        console.log(email)
-        console.log(contrasena)
-    });
-}
-
-window.addEventListener('load', registrar);
+// Obtén el botón de registro y asigna un evento click
+const registerButton = document.getElementById('register-button');
+registerButton.addEventListener('click', () => {
+  // Obtén el correo y la contraseña de los inputs
+  const email = document.getElementById('emailRegister').value;
+  const password = document.getElementById('passwordRegister').value;
+  // Llama a la función de registro
+  registerUser(email, password);
+});
