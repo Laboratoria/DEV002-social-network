06d4ba46bb2370ecf4bfeb7c978b95bd90f1c341
Paola Quiroga <paolaquirogap10@gmail.com>
2023-01-05 18:07:06 -0500

    Se hace CSS de Login

--- src/login.js
+++ src/login.js
@@ -7,7 +7,6 @@ export const login = () => {
     $imgLogo = document.createElement("img"),
     $containerICoulLogin = document.createElement("div"),
     $iCouldLogin = document.createElement("h2"),
-
     $divContainerForm = document.createElement("div"),
     $divForm = document.createElement("form"),
     $divInput = document.createElement("div"),
@@ -16,20 +15,22 @@ export const login = () => {
     $divContainerCat = document.createElement("div"),
     $imgCat = document.createElement("img"),
     $inputSignUpCat = document.createElement("input"),
-
     $divContainerSpan = document.createElement("div"),
     $spanOption = document.createElement("span"),
-
     $divContainerRegister = document.createElement("div"),
     $inputBtnGoogle = document.createElement("input"),
+    $imgGoogle = document.createElement("img"),
     $pSignUp = document.createElement("p"),
     $inputSignUp = document.createElement("input");
 
-//agregamos HTML semantico------------------------------------
+  //agregamos HTML semantico------------------------------------
 
-  $section.appendChild($divLogo).appendChild($figureLogin).appendChild($imgLogo);
+  $section
+    .appendChild($divLogo)
+    .appendChild($figureLogin)
+    .appendChild($imgLogo);
   $section.appendChild($containerICoulLogin).appendChild($iCouldLogin);
-  $section.appendChild($divContainerForm)
+  $section.appendChild($divContainerForm);
   $divContainerForm.appendChild($divForm);
   $divForm.appendChild($divInput);
   $divInput.appendChild($inputEmail);
@@ -39,21 +40,23 @@ export const login = () => {
 
   $section.appendChild($divContainerSpan).appendChild($spanOption);
 
-  $section.appendChild($divContainerRegister).appendChild($inputBtnGoogle);
-  $divContainerRegister.appendChild($pSignUp).appendChild($inputSignUp);
+  $section.appendChild($divContainerRegister);
+  $divContainerRegister.appendChild($inputBtnGoogle).appendChild($imgGoogle);
+  $divContainerRegister.appendChild($pSignUp);
+  $divContainerRegister.appendChild($inputSignUp);
   
-    //agregamos atributos----------------------------------------
-  $section.setAttribute("class", "sectionLogin")
+  //agregamos atributos----------------------------------------
+  $section.setAttribute("class", "sectionLogin");
   $figureLogin.setAttribute("class", "figureLogin");
-  $imgLogo.setAttribute("src","img/logoNewMe.png" );
+  $imgLogo.setAttribute("src", "img/logoNewMe.png");
   $imgLogo.setAttribute("alt", "Logo de la aplicacion Newme");
   $iCouldLogin.setAttribute("class", "sloganLogin");
-  $iCouldLogin.textContent = 'I could you can!'
+  $iCouldLogin.textContent = "I could you can!";
   $divContainerForm.setAttribute("class", "containerFormLogin");
   $divForm.setAttribute("id", "formLogin");
   $divForm.setAttribute("class", "formLogin");
 
-  $divInput.setAttribute("class", "problemInput")
+  $divInput.setAttribute("class", "problemInput");
 
   $inputEmail.setAttribute("type", "email");
   $inputEmail.setAttribute("class", "emailUser");
@@ -65,7 +68,7 @@ export const login = () => {
   $inputPassword.setAttribute("id", "passwordUserId");
   $inputPassword.setAttribute("placeHolder", "Password");
 
-  $divContainerCat.setAttribute("class", "ContainerCat")
+  $divContainerCat.setAttribute("class", "ContainerCat");
 
   $imgCat.setAttribute("src", "img/gato.png");
   $imgCat.setAttribute("alt", "gato negro decorativo");
@@ -74,21 +77,22 @@ export const login = () => {
   $inputSignUpCat.setAttribute("name", "btn_signUpCat");
   $inputSignUpCat.setAttribute("class", "btn_signUpCat");
   $inputSignUpCat.setAttribute("id", "btn_signUpIdCat");
-  $inputSignUpCat.setAttribute("value", "SIGN UP")
+  $inputSignUpCat.setAttribute("value", "SIGN UP");
 
   $divContainerSpan.setAttribute("class", "containerSpan");
   $spanOption.setAttribute("class", "option");
-  $spanOption.textContent = 'Or sign up With'
+  $spanOption.textContent = "Or sign up With";
 
-  $divContainerRegister.setAttribute("class", "ContainerReg")
+  $divContainerRegister.setAttribute("class", "ContainerReg");
 
   $inputBtnGoogle.setAttribute("type", "button");
   $inputBtnGoogle.setAttribute("class", "btn_google");
+  $imgGoogle.setAttribute("src", "img/LogoGoogle.png");
   $inputBtnGoogle.setAttribute("id", "btn_googleId");
   $inputBtnGoogle.setAttribute("value", "WITH GOOGLE");
 
-  $pSignUp.setAttribute("class", "register")
-  $pSignUp.textContent = "You don't have an account?"
+  $pSignUp.setAttribute("class", "register");
+  $pSignUp.textContent = "You don't have an account?";
 
   $inputSignUp.setAttribute("type", "button");
   $inputSignUp.setAttribute("class", "btn_signUp");
@@ -103,9 +107,8 @@ export const login = () => {
     exitConsult(email, password);
   });
 
-  $inputBtnGoogle.addEventListener('click', async ()=>{
-   await authGoogle();
-  }) 
+  $inputBtnGoogle.addEventListener("click", async () => {
+    await authGoogle();
+  });
   return $section;
 };
-
