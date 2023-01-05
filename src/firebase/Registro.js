3061b113b36251d2a054352449c5458781fc98e4
MiriamDiaz2306 <miriam.diaz1008@icloud.com>
2023-01-05 09:27:53 -0500

    carpetas con firebase y components

---
+++ src/firebase/Registro.js
@@ -0,0 +1,22 @@
+//Este es el punto de entrada de tu aplicacion
+import { registerUser } from './configuracion.js';
+
+// myFunction();
+const indexRegistro = document.getElementById('containerRegister');
+const indexInicioSesion = document.getElementById('container');
+const btnRegistrate = document.getElementById('registrate')
+btnRegistrate.addEventListener('click', () => {
+  indexInicioSesion.style.display = "none";
+  indexRegistro.style.display = "block";
+});
+// Obtén el botón de registro y asigna un evento click
+const registerButton = document.getElementById('register-button');
+registerButton.addEventListener('click', () => {
+  // Obtén el correo y la contraseña de los inputs
+  const email = document.getElementById('emailRegister').value;
+  const password = document.getElementById('passwordRegister').value;
+  // Llama a la función de registro
+  registerUser(email, password);
+
+});
+
