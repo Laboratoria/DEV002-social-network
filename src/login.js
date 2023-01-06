73a7a73f8b7aa6665d57c2f8281eb6983857399a
Erika Suesca <erikasuesca1234@gmail.com>
2023-01-06 13:58:32 -0500

    Se divide la pagina en dos seciones para poder mostrar bien el formulario en pagina Desktop

--- src/login.js
+++ src/login.js
@@ -7,6 +7,7 @@ export const login = () => {
     $imgLogo = document.createElement("img"),
     $containerICoulLogin = document.createElement("div"),
     $iCouldLogin = document.createElement("h2"),
+    $divContainerF = document.createElement("div"),
     $divContainerForm = document.createElement("div"),
     $divForm = document.createElement("form"),
     $divInput = document.createElement("div"),
@@ -29,8 +30,10 @@ export const login = () => {
     .appendChild($divLogo)
     .appendChild($figureLogin)
     .appendChild($imgLogo);
-  $section.appendChild($containerICoulLogin).appendChild($iCouldLogin);
-  $section.appendChild($divContainerForm);
+    $divLogo.appendChild($containerICoulLogin).appendChild($iCouldLogin)
+  
+  $section.appendChild($divContainerF);
+  $divContainerF.appendChild($divContainerForm);
   $divContainerForm.appendChild($divForm);
   $divForm.appendChild($divInput);
   $divInput.appendChild($inputEmail);
@@ -38,20 +41,23 @@ export const login = () => {
   $divForm.appendChild($divContainerCat).appendChild($imgCat);
   $divContainerCat.appendChild($inputSignUpCat);
 
-  $section.appendChild($divContainerSpan).appendChild($spanOption);
+  $divContainerF.appendChild($divContainerSpan).appendChild($spanOption);
 
-  $section.appendChild($divContainerRegister);
+  $divContainerF.appendChild($divContainerRegister);
   $divContainerRegister.appendChild($figureGoogle).appendChild($imgGoogle);
   $divContainerRegister.appendChild($inputBtnGoogle);
   $divContainerRegister.appendChild($pSignUp);
 
   //agregamos atributos----------------------------------------
   $section.setAttribute("class", "sectionLogin");
+  $divLogo.setAttribute("class", "logoNewM")
   $figureLogin.setAttribute("class", "figureLogin");
   $imgLogo.setAttribute("src", "img/logoNewMe.png");
   $imgLogo.setAttribute("alt", "Logo de la aplicacion Newme");
   $iCouldLogin.setAttribute("class", "sloganLogin");
   $iCouldLogin.textContent = "I could you can!";
+
+  $divContainerF.setAttribute("class", "containerF")
   $divContainerForm.setAttribute("class", "containerFormLogin");
   $divForm.setAttribute("id", "formLogin");
   $divForm.setAttribute("class", "formLogin");
