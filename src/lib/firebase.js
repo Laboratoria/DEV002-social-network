6dd9792b46857936810a5c1fbca725b85f07f22d
Laura Buitrago <buitrago92943@gmail.com>
2023-01-10 18:00:55 -0500

    Funcion crear cuenta y arreglo css SPA register

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 const firebaseConfig = {
   apiKey: "AIzaSyCLeEKdWYHeyxAAPoZ3-tpRs85uDEc9iYg",
   authDomain: "reading-club-43baf.firebaseapp.com",
@@ -10,7 +10,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
- export const app = initializeApp(firebaseConfig);
- 
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
 
 
--- src/lib/router.js
+++ src/lib/router.js
@@ -1,65 +1,75 @@
 import {
-    getAuth,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    signOut,
-    updateProfile,
-  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-  import { app } from './Firebase.js';
-  
-  // Initialize Firebase Authentication and get a reference to the service
-  export const auth = getAuth(app);
-  
-  // Inicia el autentificador con google
-  export const provider = new GoogleAuthProvider();
-  
-  // CREAR USUARIO CON EMAIL
-  export const createUser = (userMail, userPass, userName) => createUserWithEmailAndPassword(auth, userMail, userPass)
-    .then(() => {
-      updateProfile(getAuth().currentUser, {
-        displayName: userName,
-      });
+  getAuth,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { app } from './Firebase.js';
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Inicia el autentificador con google
+export const provider = new GoogleAuthProvider();
+
+// CREAR USUARIO CON EMAIL
+export const createUser = (userMail, userPass, userName) => createUserWithEmailAndPassword(auth, userMail, userPass)
+  .then(() => {
+    updateProfile(getAuth().currentUser, {
+      displayName: userName,
     });
-  
-  // INGRESAR CON USUARIO EXISTENTE
-  export const signUp = (userMail, userPass, onNavigate) => {
-    signInWithEmailAndPassword(auth, userMail, userPass)
-      .then((userCredential) => {
-        onNavigate('/login');
-        window.location.reload();
-        // Signed in
-        // const user = userCredential.user;
-        console.log(userCredential);
-      })
-      .catch((error) => {
-        if (error.code === 'auth/email-already-in-use') {
-          document.getElementById('errorLogin').innerHTML = 'Este correo ya está registrado';
-        } else if (error.code === 'auth/invalid-email') {
-          document.getElementById('errorLogin').innerHTML = 'El correo que ingresaste es inválido';
-        } else if (error.code) {
-          document.getElementById('errorLogin').innerHTML = 'Revisa los datos ingresados, algo no está bien';
-        }
-      });
-  };
-  
-  // INGRESAR CON GOOGLE - check
-  export const signInGoogle = async (onNavigate) => {
-    try {
-      const credentials = await signInWithPopup(auth, provider);
-      console.log('el tipo de dato es', typeof signInGoogle);
+  });
+
+// INGRESAR CON USUARIO EXISTENTE
+export const signUp = (userMail, userPass, onNavigate) => {
+  signInWithEmailAndPassword(auth, userMail, userPass)
+    .then((userCredential) => {
       onNavigate('/login');
       window.location.reload();
-    } catch (error) {
-      console.log(error);
+      // Signed in
+      // const user = userCredential.user;
+      console.log(userCredential);
+    })
+    .catch((error) => {
+      if (error.code === 'auth/email-already-in-use') {
+        document.querySelector('#errorLogin').innerHTML = 'Este correo ya está registrado';
+      } else if (error.code === 'auth/invalid-email') {
+        document.querySelector('#errorLogin').innerHTML = 'El correo que ingresaste es inválido';
+      } else if (error.code) {
+        document.querySelector('#errorLogin').innerHTML = 'Revisa los datos ingresados, algo no está bien';
+      }
+    });
+};
+
+// INGRESAR CON GOOGLE - check
+export const signInGoogle = async (onNavigate) => {
+  try {
+    const credentials = await signInWithPopup(auth, provider);
+    onNavigate('/login');
+    window.location.reload();
+  } catch (error) {
+  }
+};
+
+// FUNCIÓN DE SIGNOUT - check
+export const logOut = async () => {
+  await signOut(auth);
+};
+// FUNCION AUTHLOGIN 
+export const authLogin = () => {
+  // authLogin es la funcion para verificar usuario ya registrado y direccionarlo a nueva ruta
+  const userLogin = firebase.auth().onAuthStateChanged((user) => {
+    // Usamos el metodo onAuthStateChanged para verificar el estado de autenticacion
+    if (user) {
+      window.location.href = '#/login';
+      // en caso de que se cumpla user se direccion la ruta home
     }
-  };
-  
-  // FUNCIÓN DE SIGNOUT - check
-  export const logOut = async () => {
-    await signOut(auth);
-  };
-  
-  // funcion currentuser
-  export const currentUserInfo = () => auth.currentUser;
+  });
+  return userLogin;
+};
+
+// funcion currentuser
+export const currentUserInfo = () => auth.currentUser;
