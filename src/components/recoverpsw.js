693fbf351ecfff7002277450d87651c988ef6fa1
filletournesols <dgalarcegonzalez@gmail.com>
2023-01-11 15:53:34 -0300

    css responsive para recoverpsw.js

--- src/components/recoverpsw.js
+++ src/components/recoverpsw.js
@@ -5,13 +5,13 @@ export const RecoverPsw = () => {
     const template = `
         <section class="recover-psw" id="RecoverPsw">
             <form class="recover-psw-form" id="RecoverPswForm">
-            <div class="recover-psw-form-background">
-                <input type="recover-psw" class="recover-psw-form-input" id="RecoverPswFormInput" placeholder="hola@petblr.com" required />
-                <button class="reset-recover-psw-div-btn" id="resetRecoverPswDivBtn">RESTABLECER</button>
-                <button class="cancel-recover-psw-div-btn" id="cancelRecoverPswDivBtn">CANCELAR</button>
+                <div class="recover-psw-form-background">
+                    <input type="recover-psw" class="recover-psw-form-input" id="RecoverPswFormInput" placeholder="hola@petblr.com" required />
+                    <button class="reset-recover-psw-div-btn" id="resetRecoverPswDivBtn">RESTABLECER</button>
+                    <button class="cancel-recover-psw-div-btn" id="cancelRecoverPswDivBtn">CANCELAR</button>
                 </div>
-                </form>
-        
+            </form>
+        </section>
     `
     RecoverPswDiv.innerHTML = template
     RecoverPswDiv.querySelector('#cancelRecoverPswDivBtn').addEventListener('click', () => onNavigate('/emaillogin'))
