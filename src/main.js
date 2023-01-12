e5cccdf2e55a6453ea6242e08b74e166926f55cf
Carolina <carolinavj21@gmail.com>
2023-01-12 15:46:44 -0300

    agregamos routes en archivo main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
-
-myFunction();
+import { home } from './components/home.js';
+import { register } from './components/register.js';
+import { feed } from './lib/index.js';
 
 const rootDiv = document.getElementById("root");
+const routes = {
+    "/": home,
+    "/register": register,
+    "/feed": feed
+};
+rootDiv.appendChild(routes[window.location.pathname]());
