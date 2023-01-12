19f02edf4bbe4fe5d70dd378d6ea0b766c29e033
filletournesols <dgalarcegonzalez@gmail.com>
2023-01-12 17:46:38 -0300

    arreglo diseño responsive botones

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,17 +7,17 @@ export const Signup = () => {
     const template = `
         <section class="register" id="register">
             <form class="register-form" id="registerForm">
-            <div class="register-form-background">
-                <input type="name" class="register-form-pet-name-input" id="registerFormPetNameInput" placeholder="Nombre de usuario" required />
-                <input type="email" class="register-form-pet-email-input" id="registerFormPetEmailInput" placeholder="hola@petblr.com" required />
-                <input type="name" class="register-form-user-name-input" id="registerFormUserNameInput" placeholder="Nombre del dueño" required />
-                <input type="password" class="register-form-password-input" id="registerFormPasswordInput" placeholder="Contraseña" required />
-                <label class="register-form-psw-recomendation" id="registerFormPswRecomendation">DEBE TENER MÍNIMO 6 CARÁCTERES, UNA MAYÚSCULA Y UN NÚMERO</label>
-                <input type="password" class="register-form-repeat-password-input" id="registerFormRepeatPasswordInput"placeholder="Repetir contraseña" required />
-                <label class="register-form-repeat-psw hidden" id="registerFormRepeatPsw"> LAS CONTRASEÑAS DEBEN COINCIDIR</label>
-                <button class="register-div-btn" id="registerDivBtn">REGISTRARSE</button>
+                <div class="register-form-background">
+                    <input type="name" class="register-form-pet-name-input" id="registerFormPetNameInput" placeholder="Nombre de usuario" required />
+                    <input type="email" class="register-form-pet-email-input" id="registerFormPetEmailInput" placeholder="hola@petblr.com" required />
+                    <input type="name" class="register-form-user-name-input" id="registerFormUserNameInput" placeholder="Nombre del dueño" required />
+                    <input type="password" class="register-form-password-input" id="registerFormPasswordInput" placeholder="Contraseña" required />
+                    <label class="register-form-psw-recomendation" id="registerFormPswRecomendation">DEBE TENER MÍNIMO 6 CARÁCTERES, UNA MAYÚSCULA Y UN NÚMERO</label>
+                    <input type="password" class="register-form-repeat-password-input" id="registerFormRepeatPasswordInput"placeholder="Repetir contraseña" required />
+                    <label class="register-form-repeat-psw hidden" id="registerFormRepeatPsw"> LAS CONTRASEÑAS DEBEN COINCIDIR</label>
+                    <button class="register-div-btn" id="registerDivBtn">REGISTRARSE</button>
                 </div>
-                </form>
+            </form>
         </section>
         <section class="auth-error hidden" id="authError">
             <div class="auth-error-div">
