85776c9e979d3f9c7bcefe18401be6f2d6b705bb
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-01-13 15:23:36 -0300

    se genera vista de home y se muestra con router, main.js todo comentado

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,49 @@
 export const home = () => {
    const homeDiv = document.createElement('div');
+
+   const barraNavegacion = document.createElement('nav');
+   barraNavegacion.className = 'navBarHome';
+   homeDiv.appendChild(barraNavegacion);
+
+   const listaBotones = document.createElement('ul');
+   barraNavegacion.appendChild(listaBotones);
+
    const buttonRegister = document.createElement('li');
-   const buttonLogin = document.createElement('li'); 
+   buttonRegister.className = 'navLink';
+   buttonRegister.textContent = 'Sign in';
+   buttonRegister.id = 'botonRegistrar';
 
-buttonRegister.textContent = 'sign in';
-buttonLogin.textContent = 'sign up';
+   const buttonLogin = document.createElement('li');
+   buttonLogin.className = 'navLink';
+   buttonLogin.textContent = 'Sign up';
+   buttonLogin.id = 'botonLoguear';
 
-homeDiv.appendChild(buttonRegister);
-homeDiv.appendChild(buttonLogin);
-return  homeDiv;
+   listaBotones.appendChild(buttonRegister);
+   listaBotones.appendChild(buttonLogin);
+
+
+
+
+   const sectionDiv = document.createElement('section');
+   sectionDiv.className = 'bienvenida';
+   const bienvenidaH5 = document.createElement('h5');
+   bienvenidaH5.className = 'welcomeH5';
+   const nameApp = document.createElement('h1');
+   nameApp.className = 'nameAplication';
+
+   bienvenidaH5.textContent = 'Welcome t💗';
+   nameApp.textContent = "Dad's Power";
+   sectionDiv.appendChild(bienvenidaH5);
+   sectionDiv.appendChild(nameApp);
+
+   homeDiv.appendChild(sectionDiv);
+
+   const imagenInicio = document.createElement('img');
+   imagenInicio.src = '/components/imagen/inicioApp.png';
+   imagenInicio.className = 'imgInicio';
+
+   homeDiv.appendChild(imagenInicio);
+
+
+   return homeDiv;
 }   
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,12 +1,12 @@
-import {home} from '/components/home.js';
-import {register} from '/components/register.js'
-import {login} from '/components/login.js'
+import {home} from '../components/Home.js';
+// import {register} from '../components/register.js';
+// import {login} from '../components/login.js';
 
 const rootDiv = document.getElementById('root');
 const routes = {
 '/':home,
-'/register': register,
-'/login': login,
+// '/register': register,
+// '/login': login,
 };
 
 const componentes = routes[window.location.pathname];
