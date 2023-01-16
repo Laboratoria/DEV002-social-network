9d7d48d4ac709f900e923dc094a2c2076613bd86
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-01-16 15:21:48 -0300

    register sin funcionar, pruebas

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import { onNavigate } from '../js/routes.js'
 export const home = () => {
    const homeDiv = document.createElement('div');
 
@@ -17,6 +18,9 @@ export const home = () => {
    buttonLogin.className = 'navLink';
    buttonLogin.textContent = 'Sign up';
    buttonLogin.id = 'botonLoguear';
+
+   buttonRegister.addEventListener('click', () => onNavigate('/register'));
+
 
    listaBotones.appendChild(buttonRegister);
    listaBotones.appendChild(buttonLogin);
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,12 +1,20 @@
 import {home} from '../components/Home.js';
-// import {register} from '../components/register.js';
+import {register} from '../components/register.js';
 // import {login} from '../components/login.js';
 
 const rootDiv = document.getElementById('root');
 const routes = {
 '/':home,
-// '/register': register,
+'/register': register,
 // '/login': login,
+};
+
+export const onNavigate = (pathname) => {
+    window.history.pushState(
+        {},
+        pathname,
+        window.location.origin + pathname,
+    );
 };
 
 const componentes = routes[window.location.pathname];
