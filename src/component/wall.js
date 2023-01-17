c02823b353f96c9e016ae39d58a68de28cf8b5c2
sanlorenita-18 <sanlorenita-18@hotmail.com>
2023-01-16 20:40:31 -0500

    continuacion del muro

--- src/component/wall.js
+++ src/component/wall.js
@@ -1,16 +1,37 @@
 export const wall = () => {
-const $sectionW = document.createElement("section"),
-$divContainerWall = document.createElement("div"),
+const $sectionW = document.createElement("section"),  //padre de tres
+$divContainerWall = document.createElement("div"),  //hijo 1
 $divSaludo = document.createElement("div"),
 $imgLogoW = document.createElement("img"),
 $h2name = document.createElement("h2"),
-$divPost = document.createElement("div"),
-$h3Post = document.createElement("h3"),
+
+$containerOrange = document.createElement("div"),  //hijo 2
+
+$divRightOrange = document.createElement("div"),  
+$divImgWall = document.createElement("div"),
 $imgWall = document.createElement("img"),
-$divPublication = document.createElement("div"),
-$divPublicUser = document.createElement("div"),
-$divReactions = document.createElement("div"),
-$divSignOffCat = document.createElement("div"),
+
+$divLeftOrange = document.createElement("div"),
+$divH3Post = document.createElement("div"),
+$h3Post = document.createElement("h3"),
+$containerFormWall = document.createElement("div"),
+$formWall =document.createElement("form"),
+$inputPublication = document.createElement("input"),
+$inputBtnWall = document.createElement("input"),
+
+$containerPublication = document.createElement("div"), // hijo de section y padre de dos 2
+$divPublicUser = document.createElement("div"), //padre gris y blanca   hijo 1
+$divPublicU = document.createElement("div"), //parte gris
+$divReactions = document.createElement("div"), //parte blanca padre de tres
+$divReactionLikes = document.createElement("div"),  //padrecorazon
+$imgHeart = document.createElement("img"),
+$inputReactionLikes = document.createElement("input"),
+$divReactionEdit = document.createElement("div"),  //padreeditar
+$inputReactionEdit = document.createElement("input"),
+$divReactionDelete = document.createElement("div"),  //padre eliminar
+$inputReactionDelete = document.createElement("input"),
+
+$divSignOffCat = document.createElement("div"),  //hijo 2
 $btnSingOff = document.createElement("button"),
 $imgWallCat = document.createElement("img")
 
@@ -21,16 +42,38 @@ $divContainerWall.appendChild($divSaludo);
 $divSaludo.appendChild($imgLogoW);
 $divSaludo.appendChild($h2name);
 
-$divContainerWall.appendChild($divPost);
-$divPost.appendChild($h3Post);
-$divPost.appendChild($imgWall);
+$sectionW.appendChild($containerOrange);
+$containerOrange.appendChild($divRightOrange);
+$divRightOrange.appendChild($divImgWall);
+$divImgWall.appendChild($imgWall);
 
-$sectionW.appendChild($divPublication);
-$divPublication.appendChild($divPublicUser);
-$divPublication.appendChild($divReactions);
+$containerOrange.appendChild($divLeftOrange);
+$divLeftOrange.appendChild($divH3Post);
+$divH3Post.appendChild($h3Post);
+$divLeftOrange.appendChild($containerFormWall);
+$containerFormWall.appendChild($formWall);
+$formWall.appendChild($inputPublication);
+$formWall.appendChild($inputBtnWall);
 
-$sectionW.appendChild($divSignOffCat);
+$sectionW.appendChild($containerPublication);
+$containerPublication.appendChild($divPublicUser);
+$divPublicUser.appendChild($divPublicU);
+$divPublicUser.appendChild($divReactions);
+
+$divReactions.appendChild($divReactionLikes);
+$divReactionLikes.appendChild($imgHeart);
+$divReactionLikes.appendChild($inputReactionLikes)
+
+$divReactions.appendChild($divReactionEdit);
+$divReactionEdit.appendChild($inputReactionEdit);
+
+$divReactions.appendChild($divReactionDelete);
+$divReactionDelete.appendChild($inputReactionDelete);
+
+$containerPublication.appendChild($divSignOffCat);
 $divSignOffCat.appendChild($btnSingOff);
 $divSignOffCat.appendChild($imgWallCat);
+
+//---------- ATRIBUTOS -------------//
 
 }
