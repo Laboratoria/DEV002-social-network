35e436f7af5332497835f7cfdafabab39f5f6d71
katterin olivares <katterinolivaresalcalde28@gmail.com>
2023-01-17 21:03:43 -0500

    trabajando en firebase

---
+++ src/lib/firebase/configFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyDFACgKOX8rAKux2bzuLQsbQoS6tZ19O_A",
+authDomain: "que-comemos-hoy-3ebf5.firebaseapp.com",
+projectId: "que-comemos-hoy-3ebf5",
+storageBucket: "que-comemos-hoy-3ebf5.appspot.com",
+messagingSenderId: "684300619009",
+appId: "1:684300619009:web:abc6371348575d591a9f00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
