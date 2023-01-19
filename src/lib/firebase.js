2655bc617f1cf0feb91df1358c1f63038f640f83
Paola Quiroga <paolaquirogap10@gmail.com>
2023-01-19 11:29:31 -0500

    Se arreglan ultimos errores y volvemos a firebase CDN

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,13 +1,17 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from './firebase/app';
+// eslint-disable-next-line import/no-unresolved
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 // Consultas firebase
+// eslint-disable-next-line import/no-unresolved
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
-} from './firebase/auth';
+}
+// eslint-disable-next-line import/no-unresolved
+  from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
 
 import firebaseConfig from './firebaseconfig.js';
 
