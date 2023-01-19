e8b0e41cd66d1a1558c64929e078b15159745425
vickymendoza <vickymendoza90@gmail.com>
2023-01-19 09:00:51 -0500

    se crean archivos currentUser y se revive setPersistence

--- src/app/setPersistence.js
+++ src/app/setPersistence.js
@@ -1,12 +1,11 @@
 import { firebaseAuth, setPersistence, signInWithRedirect, inMemoryPersistence, GoogleAuthProvider } from './firebase.js';
-//Este será el archivo para el comportamiento de persistencia. lo que hace es que nos permite 
-//"saber que pasa cierra sesión o qué pasa sí sólo cierra la pestaña"
-
+//Este será el archivo para el comportamiento de persistencia. lo que hace es que nos permite
+//“saber que pasa cierra sesión o qué pasa sí sólo cierra la pestaña”
 // setPersistence(firebaseAuth, inMemoryPersistence)
 //   .then(() => {
 //     const provider = new GoogleAuthProvider();
 //     // In memory persistence will be applied to the signed in Google user
-//     // even though the persistence was set to 'none' and a page redirect
+//     // even though the persistence was set to ‘none’ and a page redirect
 //     // occurred.
 //     return signInWithRedirect(firebaseAuth, provider);
 //   })
@@ -17,8 +16,7 @@ import { firebaseAuth, setPersistence, signInWithRedirect, inMemoryPersistence, 
 //     const errorMessage = error.message;
 //     console.log({errorMessage})
 //   });
-
-  export const setPersistenceFun = async (e)=>{
+export const setPersistenceFun = async (e)=>{
     e.preventDefault();
     try {
       await setPersistence(firebaseAuth, inMemoryPersistence)
