ad452b879d0e2b33b077a6b3c2c660e02d8228e4
Fernanda Guzman <fda.guzmann@gmail.com>
2023-01-20 11:50:24 -0400

    getdocs

--- src/app/getDoc.js
+++ src/app/getDoc.js
@@ -1,35 +1,68 @@
-import { database, addDoc, getTask, onSnapshot } from './firebase.js';
+import { database, addDoc, collection, getDocs } from './firebase.js';
 
-const saveTask = (title, description) => {
-  return addDoc(collection(database , 'tasks'), {title, description })
+const saveTask = (description) => {
+  return addDoc(collection(database , 'posts'), {description })
+
 };
 
-const taskForm = document.getElementById("taskForm")
+// const taskForm = document.getElementById("taskForm")
 const taskContainer = document.getElementById('taskContainer')
 
-window.addEventListener("DOMContentLoaded", async () => {
-  const querySnapshot = await getTask()
-  onSnapshot(collection (database, 'task'),(querySnapshot) => {
-    let html = '';
-    querySnapshot.forEach((doc) => {
-      const task = doc.data();
-      html += `
-      <div>
-      <h3>${task.title}</h3>
-      <p>${task.description}</p>
-      </div>
-      `;
-    });
-    taskContainer.innerHTML = html;
+// const getPosts = ( ) => {
+//   getDocs(collection(database , 'posts'))
+// }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const querySnapshot = await getDocs(collection(database, "posts"));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
   });
-
-  taskForm.addEventListener ('submit', (e) => {
-    e.preventDefault()
-    const description = taskForm["postsTextArea"]
-
-    console.log(description)
-
-    saveTask(description.value)
-    taskForm.reset()
-  })
+   
 })
+
+// taskForm.addEventListener ('submit', (e) => {
+//   e.preventDefault()
+//   const description = taskForm["postsTextArea"]
+//   console.log(description.value)
+// })
+
+// const createPostBtn =document.getElementById ("createPostBtn") 
+// createPostBtn.addEventListener("click", (e) => {
+//   e.preventDefault()
+//   taskForm.addEventListener ('submit', (e) => {
+//     e.preventDefault()
+//     const description = taskForm["postsTextArea"]
+//     console.log(description.value)
+//   })
+// })
+
+// window.addEventListener("DOMContentLoaded", async () => {
+//   const querySnapshot = await getTask()
+//   onSnapshot(collection (database, 'task'),(querySnapshot) => {
+//     let html = '';
+//     querySnapshot.forEach((doc) => {
+//       const task = doc.data();
+//       html += `
+//       <div>
+//       <h3>${task.title}</h3>
+//       <p>${task.description}</p>
+//       </div>
+//       `;
+//     });
+//     taskContainer.innerHTML = html;
+//   });
+
+  // taskForm.addEventListener ('submit', (e) => {
+  //   e.preventDefault()
+  //   const description = taskForm["postsTextArea"]
+
+  //   console.log(description)
+
+  //   saveTask(description.value)
+  //   taskForm.reset()
+  // })
+// })
+
+export { saveTask } 
+
