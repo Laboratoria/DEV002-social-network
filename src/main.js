857f71d4194f8e0ff023ff34e411cc0ff6715ba1
BelenCiel <belenaranjonoeck@icloud.com>
2023-01-27 14:00:27 -0300

    trabaje en css vista movile

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
+/* eslint-disable import/no-cycle */
 import { login } from './components/login.js';
 import { home } from './components/home.js';
 import { register } from './components/register.js';
 
-const root = document.getElementById('root') /* contenido de html */
+const root = document.getElementById('root'); /* contenido de html */
 
-
-const routes = { 
-    '/': home, 
-    '/Login': login,
-    '/Register': register,
+const routes = {
+  '/': home,
+  '/Login': login,
+  '/Register': register,
 };
 
 export const navigateRoutes = (pathname) => {
-    window.history.pushState ( /* to load new HTML pages */
-    {}, /*state -objeto de entrada del historial- */
-    pathname, /*title*/
+  window.history.pushState( /* to load new HTML pages */
+    {}, /* state -objeto de entrada del historial- */
+    pathname, /* title */
     window.location.origin + pathname, /*url*/
-    );
-    while (root.firstChild){ /* defino que routes no se repita al clickear btn, y solo muestre el contenido clickeado*/
-        root.removeChild(root.firstChild);
-    }
-    root.appendChild(routes[pathname]());
+  );
+while(root.firstChild) { /* defino que routes no se repita al clickear btn, y solo muestre el contenido clickeado*/
+root.removeChild(root.firstChild);
+}
+  root.appendChild(routes[pathname]());
 };
 
-const component = routes[window.location.pathname]() /*variable que contiene el pathname -nombre de la ruta-*/
+const component = routes[window.location.pathname]() /* variable que contiene el pathname -nombre de la ruta- */
 console.log(component)
-  root.appendChild(component); /*Me muestra routes y su contenido*/
+root.appendChild(component); /* Me muestra routes y su contenido */
