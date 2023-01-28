ab136c830dc113bb7d66a4db32b16ee7068de896
MiriamDiaz2306 <miriam.diaz1008@icloud.com>
2023-01-28 12:01:14 -0600

    Se muestran las publicaciones con fecha y hora pero sin orden

--- src/firebase/configuracion.js
+++ src/firebase/configuracion.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line import/no-unresolved
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
 import { createUserWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js';
-import { getFirestore, collection, doc, addDoc, getDoc, getDocs, deleteDoc, updateDoc, Timestamp, onSnapshot, arrayUnion,
+import { getFirestore, collection, doc, addDoc, getDoc, getDocs, deleteDoc, updateDoc, Timestamp, query, orderBy, onSnapshot, arrayUnion,
   arrayRemove, } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
 // const auth = getAuth();
 
@@ -39,6 +39,11 @@ export const onGetTasks = (callback) => onSnapshot(collection(db, 'tasks'), call
 export const deleteTask = id => deleteDoc(doc(db, 'tasks', id));
 export const getTask = id => getDoc(doc(db, "tasks", id));
 export const updateTask =  (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
+export const readingPost = (callback) => {
+  const q = query(collection(db, 'tasks'), orderBy('date', 'desc'));
+  onSnapshot(q, callback);
+};
+
 
 
 // Crea una función para registrar usuarios
@@ -133,4 +138,5 @@ export {
   onSnapshot,
   arrayUnion,
   arrayRemove,
+  Timestamp
 };
