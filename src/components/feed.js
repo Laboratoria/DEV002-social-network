1e9280958252e9495137118ea9bbfd037e1da560
Carolina <carolinavj21@gmail.com>
2023-01-30 15:34:13 -0300

    agregando funciones de firestore

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,24 +1,25 @@
 import { toNavigate } from "../main.js";
 import { register } from "../components/register.js"
 import { auth, logout } from "../Firebase/firebase.js";
+import { addPost, postCollection, userCollection } from "../Firebase/firestore.js";
 
 export const feed = () => {
     //Creamos elementos del Feed
     const feedDiv = document.createElement("div");
     feedDiv.classList = "feedDiv"
     const header = document.createElement("div");
-    
+
     const imgHeader = document.createElement("img");
     imgHeader.src = "../img/Logo VeganShip.png"
     imgHeader.classList = "imgHeader"
-    
+
     const inputSearchHeader = document.createElement("input");
     inputSearchHeader.placeholder = "tu búsqueda"
 
     const buttonSignOut = document.createElement("button");
     buttonSignOut.textContent = "Cerrar Sesión";
 
-    const newPostContainer = document.createElement("section");
+    const newPostContainer = document.createElement("form");
     newPostContainer.classList = "newPostContainer";
     const newPostLocation = document.createElement("input");
     newPostLocation.placeholder = "ubicación"
@@ -29,6 +30,7 @@ export const feed = () => {
     const newPostContent = document.createElement("textarea");
     newPostContent.classList = "newPostContent"
     const newPostButton = document.createElement("button");
+    newPostButton.textContent = "publicar";
     const postFeed = document.createElement("section");
     const post = document.createElement("article");
     const postHeader = document.createElement("div");
@@ -36,7 +38,7 @@ export const feed = () => {
     const imgProfilePost = document.createElement("img");
     imgProfilePost.src = "../img/sandia-logo.png";
     imgProfilePost.classList = "imgProfilePost";
-
+    
     const postUserName = document.createElement("h3");
     postUserName.textContent = "Vaquita Vegana";
     const postLocation = document.createElement("h4");
@@ -52,6 +54,7 @@ export const feed = () => {
     postContent.textContent = "receta de dobladitas";
     const likeButton = document.createElement("button");
     likeButton.textContent = "like";
+
 
     feedDiv.appendChild(header);
     header.appendChild(imgHeader);
@@ -73,20 +76,34 @@ export const feed = () => {
     postContentContainer.appendChild(postTag);
     postContentContainer.appendChild(postContent);
     postContentContainer.appendChild(likeButton);
-    
 
-    
+
+
 
     buttonSignOut.addEventListener("click", () => toNavigate("/"));
     buttonSignOut.addEventListener("click", async (e) => {
-            e.preventDefault() //cancela comportamiento por defecto de refrescar la pagina
-            try {
-                await logout(auth)
-            } catch (error) {
-                console.log(error)
-            }
+        e.preventDefault() //cancela comportamiento por defecto de refrescar la pagina
+        try {
+            await logout(auth)
             toNavigate("/");
-        })
+        } catch (error) {
+            console.log(error)
+        }
+        
+    })
 
-        return feedDiv;
-    }
+    newPostButton.addEventListener("submit", async (e) => {
+        e.preventDefault()
+        try{
+            const postdescription = newPostContent.value;
+            await addPost(postdescription)
+            console.log(postdescription);
+            newPostContainer.reset();
+        }catch (error) {
+            console.log(error)
+        }
+    })
+    
+
+    return feedDiv;
+}
