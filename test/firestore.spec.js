2dd5847b9203676126763e5b2e3c3d5cb4bcbbc6
Daniela Castro <dgonzalezcs@outlook.com>
2023-02-05 20:38:43 -0600

    24 de 33 tests pasan pero aún no se muestra la cobertura

--- test/firestore.spec.js
+++ test/firestore.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-console */
 /* eslint-disable no-shadow */
 /* eslint-disable arrow-body-style */
@@ -9,23 +10,23 @@
 // Test para Firestore
 
 import {
-    deleteTask,
-    getTasks,
+    arrayRemove,
+    onSnapshot,
     updateTask,
+    deleteTask,
+    arrayUnion,
     collection,
     deleteDoc,
-    doc,
-    getDoc,
     updateDoc,
+    getTasks,
+    saveTask,
+    saveUser,
     dislike,
     tapLike,
-    saveTask,
-    onSnapshot,
-    addDoc,
-    arrayRemove,
-    arrayUnion,
-    db,
     getTask,
+    getDoc,
+    doc,
+    db,
 } from '../src/firebase/configuracion.js';
 
 jest.mock('../src/firebase/configuracion.js', () => {
@@ -33,9 +34,10 @@ jest.mock('../src/firebase/configuracion.js', () => {
         auth: jest.fn(() => {
             return { auth: 'TEST' };
         }),
+
         addDoc: jest.fn(),
         collection: jest.fn(),
-        db: jest.fn(),
+        conFirestore: jest.fn(),
         onSnapshot: jest.fn(),
         deleteDoc: jest.fn(),
         getDoc: jest.fn(),
@@ -44,34 +46,129 @@ jest.mock('../src/firebase/configuracion.js', () => {
         arrayUnion: jest.fn(),
         arrayRemove: jest.fn(),
 
+        saveTask: jest.fn((description, likes) => {
+            if (!description || likes === undefined) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        saveUser: jest.fn((uid, email, pais) => {
+            if (!uid || !email || !pais) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        getTasks: jest.fn((collection) => {
+            if (!collection) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        getTask: jest.fn((id) => {
+            if (!id) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        deleteTask: jest.fn((id) => {
+            if (!id) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        updateTask: jest.fn((id, newFields) => {
+            if (!id || !newFields) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        dateTask: jest.fn((querySnapshot) => {
+            if (!querySnapshot) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        dislike: jest.fn((likes) => {
+            if (likes === undefined) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
+
+        tapLike: jest.fn((id, newLike) => {
+            if (!id || !newLike) {
+                throw new Error('ERROR');
+            }
+            return Promise.resolve({ user: 'admin' });
+        }),
     }
 });
 
+// describe('Test for saveTask', () => {
+//     it('saveTask should be a function', () => {
+//         expect(typeof saveTask).toBe('function')
+//     });
+
+//     it('saveTask should call addDoc with its parameters.', () => {
+//         saveTask('test description', []);
+//         expect(addDoc).toHaveBeenCalledWith(
+//             collection(db, 'tasks'),
+//             {
+//                 description: 'test description',
+//                 likes: [],
+//             },
+//         )
+//     });
+// });
+
 describe('Test for saveTask', () => {
     it('saveTask should be a function', () => {
-        expect(typeof saveTask).toBe('function')
+      expect(typeof saveTask).toBe('function')
     });
+
     it('saveTask should call addDoc with its parameters.', () => {
+      saveTask('test description', []);
+      expect(saveTask).toHaveBeenCalledWith(
+        collection(db, 'tasks'),
+        {
+        description: 'test description', likes: [],
+        },
+      )
+    });
+  });
+
+describe('Test for saveUser', () => {
+    it('saveUser should be a function', () => {
+        expect(typeof saveUser).toBe('function')
+    });
+    it('saveUser should call addDoc with its parameters.', () => {
         saveTask('test description');
-        expect(addDoc).toHaveBeenCalledWith(
-            collection(db, 'tasks'),
-            { description: 'test description', likes: [] },
+        expect(saveUser).toHaveBeenCalledWith(
+            collection(db, 'users'),
+            { uid: 'xErqSqwE', email: 'admin@test.com', likes: [] },
         )
     })
 });
 
-describe('Test for getTask', () => {
-    it('onGetTasks should be a function', () => {
+describe('Test for getTasks', () => {
+    it('GetTasks should be a function', () => {
         expect(typeof getTasks).toBe('function')
     });
-    it('getTask should call onSnapshot and its parameters', () => {
+    it('getTasks should call onSnapshot and its parameters', () => {
         getTask('callback');
         expect(onSnapshot).toHaveBeenCalledWith(collection(db, 'tasks'), 'callback')
     })
 });
 
 describe('Test for deleteTask', () => {
-    it('deleteTask shoudl be a function', () => {
+    it('deleteTask should be a function', () => {
         expect(typeof deleteTask).toBe('function')
     });
     it('deleteTask should call deleteDco with its parameters', () => {
@@ -80,7 +177,7 @@ describe('Test for deleteTask', () => {
     })
 });
 
-describe('Test for getTask2', () => {
+describe('Test for getTask', () => {
     it('getTask should be a function', () => {
         expect(typeof getTask).toBe('function')
     });
@@ -91,34 +188,44 @@ describe('Test for getTask2', () => {
 });
 
 describe('Test for updateTask', () => {
-    it('updateTask shoudl be a function', () => {
+    it('updateTask should be a function', () => {
         expect(typeof updateTask).toBe('function')
     });
-    it('updateTask shoudl call updateDoc with its parameters.', () => {
+    it('updateTask should call updateDoc with its parameters.', () => {
+        updateTask('id', 'edited post');
+        expect(updateDoc).toHaveBeenCalledWith(doc(db, 'tasks', 'id'), 'edited post')
+    })
+});
+
+describe('Test for dateTask', () => {
+    it('dateTask should be a function', () => {
+        expect(typeof updateTask).toBe('function')
+    });
+    it('updateTask should call updateDoc with its parameters.', () => {
         updateTask('id', 'edited post');
         expect(updateDoc).toHaveBeenCalledWith(doc(db, 'tasks', 'id'), 'edited post')
     })
 });
 
 describe('Test for tapLike', () => {
-    it('tapLike shoudl be a function', () => {
+    it('tapLike should be a function', () => {
         expect(typeof tapLike).toBe('function')
     });
-    it('tapLike  shoudl call updateDoc and its parameters and add a like to a post', () => {
+    it('tapLike  should call updateDoc and its parameters and add a like to a post', () => {
         tapLike('id1', 'user');
-        expect(updateDoc).toHaveBeenCalledWith(doc(db, 'tasks', 'id1'), {
+        expect(updateDoc).toHaveBeenCalledWith(doc(db, 'tasks', 'id'), {
             likes: arrayUnion('user'),
         })
     })
 });
 
 describe('Test for dislike', () => {
-    it('dislike debería ser una funcion', () => {
+    it('should be a function', () => {
         expect(typeof dislike).toBe('function')
     });
-    it('dislike  should debería called updateDoc with its parameters and remove a like from a post.', () => {
-        dislike('id1', 'user');
-        expect(updateDoc).toHaveBeenCalledWith(doc(db, 'tasks', 'id1'), {
+    it('should call updateDoc with its parameters and remove a like from a post.', () => {
+        dislike('id', 'user');
+        expect(updateDoc).toHaveBeenCalledWith(doc(db, 'tasks', 'id'), {
             likes: arrayRemove('user'),
         })
     })
