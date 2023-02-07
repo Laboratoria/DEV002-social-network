b146eb1b2ebd7ef2ee1fe4e0cebb9cce15525381
katterin olivares <katterinolivaresalcalde28@gmail.com>
2023-02-07 16:29:26 -0500

    error

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,15 @@
 import { createUserWithEmailAndPassword, verifiedEmail }
   from '../src/lib/firebase/registerFir.js';
 
+  jest.mock('../src/lib/firebase/registerFir.js', () => ({
+    createUserWithEmailAndPassword: jest.fn((email, password) => {
+      if (!email || !password) {
+        throw new Error('ERROR');
+      }
+      return Promise.resolve({ userCredential: 'admin' });
+    }),
+  }));
+
 describe('test para la funcion de login', () => {
   const email = 'petblr@test.com';
   const password = 'Petblrlomejor123';
