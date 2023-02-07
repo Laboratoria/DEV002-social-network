ae5a743f3c1e2f55c61aee691709662faca0a461
katterin olivares <katterinolivaresalcalde28@gmail.com>
2023-02-07 09:15:53 -0500

    resolucion de algunos errores del test

--- src/components/home.js
+++ src/components/home.js
@@ -1,39 +1,39 @@
-import {onNavigate} from '../main.js';
+// eslint-disable-next-line import/no-cycle
+import { onNavigate } from '../main.js';
 
 export const home = () => {
-    const homeDiv = document.createElement('div');
-    const sectionDiv = document.createElement('section');
-    const nombreApp= document.createElement('h2');
-    const imageLogo = document.createElement('img');
-    const bienvenidaH1= document.createElement('h3');
-    const buttonRegister = document.createElement('button');
-    const buttonLogin = document.createElement('button');
+  const homeDiv = document.createElement('div');
+  const sectionDiv = document.createElement('section');
+  const nombreApp = document.createElement('h2');
+  const imageLogo = document.createElement('img');
+  const bienvenidaH1 = document.createElement('h3');
+  const buttonRegister = document.createElement('button');
+  const buttonLogin = document.createElement('button');
 
-    sectionDiv.className= 'sectionDiv';
-    nombreApp.className='nombreAplicacion';
-    imageLogo.className = 'imageLogo';
-    bienvenidaH1.className= 'welcomeh1';
+  sectionDiv.className = 'sectionDiv';
+  nombreApp.className = 'nombreAplicacion';
+  imageLogo.className = 'imageLogo';
+  bienvenidaH1.className = 'welcomeh1';
 
-    imageLogo.src = "images/logo-quecomemoshoy.png";
+  imageLogo.src = 'images/logo-quecomemoshoy.png';
 
-    
-    buttonLogin.id= 'buttonLogin';
-    buttonRegister.id= 'buttonRegister';
+  buttonLogin.id = 'buttonLogin';
+  buttonRegister.id = 'buttonRegister';
 
-    nombreApp.textContent="INSPIRACIÓN PARA TUS COMIDAS";
-    bienvenidaH1.textContent='Bienvenido';
-    buttonRegister.textContent= 'Registrate';
-    buttonLogin.textContent= 'Inicia sesion';
+  nombreApp.textContent = 'INSPIRACIÓN PARA TUS COMIDAS';
+  bienvenidaH1.textContent = 'Bienvenido';
+  buttonRegister.textContent = 'Registrate';
+  buttonLogin.textContent = 'Inicia sesion';
 
-    buttonRegister.addEventListener('click',() => onNavigate('/register'));
-    buttonLogin.addEventListener('click',() => onNavigate ('/login'));
+  buttonRegister.addEventListener('click', () => onNavigate('/register'));
+  buttonLogin.addEventListener('click', () => onNavigate('/login'));
 
-    homeDiv.appendChild(sectionDiv);
-    sectionDiv.appendChild(nombreApp);
-    sectionDiv.appendChild(imageLogo);
-    homeDiv.appendChild(bienvenidaH1);
-    homeDiv.appendChild(buttonRegister);
-    homeDiv.appendChild(buttonLogin);
+  homeDiv.appendChild(sectionDiv);
+  sectionDiv.appendChild(nombreApp);
+  sectionDiv.appendChild(imageLogo);
+  homeDiv.appendChild(bienvenidaH1);
+  homeDiv.appendChild(buttonRegister);
+  homeDiv.appendChild(buttonLogin);
 
-    return homeDiv;
+  return homeDiv;
 };
