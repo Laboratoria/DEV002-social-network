9c1ca824367e8efc47f360fc00894033bbde6434
Daniela Castro <dgonzalezcs@outlook.com>
2023-02-07 12:31:36 -0600

    contenedor de likes centrado

--- src/firebase/configuracion.js
+++ src/firebase/configuracion.js
@@ -35,8 +35,6 @@ import {
 // eslint-disable-next-line import/no-unresolved
 } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
 
-// const auth = getAuth();
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAAHfvbH9LChUrOwAbR4cydwCsmHa7Q330',
@@ -47,12 +45,13 @@ const firebaseConfig = {
   appId: '1:15257223280:web:eecc0cb646124a2f42b4b5',
   measurementId: 'G-4W8ETMYH7S',
 };
+
 // Inicializa la aplicación de Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider(app);
-export const db = getFirestore(app);
-export const user = () => auth.currentUser;
+export const app = initializeApp(firebaseConfig); // Inicializa una instancia de Firebase
+export const auth = getAuth(app); // Obtiene un servicio de autenticación de Firebase a partir de la instancia inicializada
+export const provider = new GoogleAuthProvider(app);// Crea un proveedor de autenticación de Google a partir de la instancia de Firebase.
+export const db = getFirestore(app);// Obtiene un servicio de base de datos de Firestore de Firebase a partir de la instancia inicializada.
+export const user = () => auth.currentUser;// Define una función que devuelve el usuario actualmente autenticado en Firebase.
 
 export const saveTask = (description) => addDoc(collection(db, 'tasks'), {
   description,
@@ -70,13 +69,14 @@ export const saveUser = (name, uid, email, pais) => addDoc(collection(db, 'users
   createdDateTime: Timestamp.fromDate(new Date()),
 });
 
-export const getTasks = () => getDocs(collection(db, 'tasks'));
-export const deleteTask = (id) => deleteDoc(doc(db, 'tasks', id));
-export const getTask = (id) => getDoc(doc(db, 'tasks', id));
-export const updateTask = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);
-export const dateTask = (querySnapshot) => {
-  const q = query(collection(db, 'tasks'), orderBy('createdDateTime', 'desc'));
-  onSnapshot(q, querySnapshot);
+// Estas funciones realizan una operación específica en la colección de tareas en Firestore.
+export const getTasks = () => getDocs(collection(db, 'tasks')); // Obtiene todos los documentos de la colección de tareas utilizando la función getDocs.
+export const deleteTask = (id) => deleteDoc(doc(db, 'tasks', id)); // Elimina un documento específico de la colección de tareas utilizando la función deleteDoc con su id.
+export const getTask = (id) => getDoc(doc(db, 'tasks', id));// Obtiene un documento específico de la colección de tareas utilizando la función getDoc con el id.
+export const updateTask = (id, newFields) => updateDoc(doc(db, 'tasks', id), newFields);// Actualiza un documento específico de la colección de tareas utilizando la función updateDoc.
+export const dateTask = (querySnapshot) => { // 0rdena todos los documentos en la colección de tareas por fecha y hora de creación en orden descendente utilizando la función query y orderBy
+  const q = query(collection(db, 'tasks'), orderBy('createdDateTime', 'desc'));// escucha los cambios de la coleccion con la función onSnapshot
+  onSnapshot(q, querySnapshot);// querySnapshot contiene información sobre los documentos devueltos por la consulta en tiempo real.
 };
 
 // Create new users
@@ -96,7 +96,7 @@ export function registerUser(email, password, name, pais, callback) {
       user.displayName = name;
       // console.log(user, userId);
       saveUser(user.displayName, userId, email, pais);
-      callback(true); // permite que la función Register User proporcione un resultado a través de una función que se pasa como argumento.
+      callback(true); // / Se llama una función de callback para indicar que el registro se ha realizado correctamente.
     })
     .catch((error) => {
       console.error(error.code);
@@ -124,7 +124,7 @@ export async function inicioDeSesionEmail(email, password) {
     const user = userCredential.user;
     const userId = user.uid;
     console.log(user, userId);
-    return true;
+    return true; // Si la operación es exitosa, la función imprime un mensaje "signed in" y devuelve true.
   } catch (error) {
     if (error.code === 'auth/email-already-in-use') {
       alert('Este correo ya está registrado');
@@ -157,7 +157,7 @@ export const authGoogle = async () => {
 
 // Cerrar sesión
 
-export const signOutFirebase = (auth) => auth.signOut();
+export const signOutFirebase = (auth) => auth.signOut(); // llama a su método signOut() que termina la sesión actual.
 
 export const onAuth = (auth) => {
   auth.onAuthStateChanged((user) => {
@@ -173,18 +173,18 @@ export const onAuth = (auth) => {
 // Like function
 
 export const tapLike = (id, newLike) => {
-  updateDoc(doc(db, 'tasks', id), {
-    likes:
-      arrayUnion(
+  updateDoc(doc(db, 'tasks', id), { // Actualiza el documento en la colección "tasks" con el
+    likes: // identificador "id" agregando un nuevo elemento "newLike" a la lista de "likes"
+      arrayUnion( // Permite agregar un nuevo elemento a un campo en la base de datos
         newLike,
       ),
   });
 };
 
-export const dislike = (id, oldLike) => {
-  updateDoc(doc(db, 'tasks', id), {
+export const dislike = (id, oldLike) => { // Se realiza la operación opuesta, eliminando un elemento "oldLike" de la lista de "likes".
+  updateDoc(doc(db, 'tasks', id), { // updateDoc actualiza el documento en la base de datos y doc toma como argumentos db, task y id
     likes:
-      arrayRemove(
+      arrayRemove( // Permite eliminar un elemento en la base de datos.
         oldLike,
       ),
   });
@@ -192,23 +192,22 @@ export const dislike = (id, oldLike) => {
 
 export {
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+  getFirestore,
+  arrayRemove,
   collection,
-  addDoc,
-  getDocs,
-  deleteDoc,
-  doc,
-  getDoc,
-  updateDoc,
   onSnapshot,
   arrayUnion,
-  arrayRemove,
+  updateDoc,
+  deleteDoc,
   Timestamp,
-  updateProfile,
-  onAuthStateChanged,
   signOut,
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  getFirestore,
-
+  getDocs,
+  getDoc,
+  addDoc,
+  doc,
 };
