83c9a8cb0ab08098cbf9f077dad55f409321fdfb
RafaelEspinoza <grafa884@gmail.com>
2023-02-09 12:19:02 -0500

    test no completados

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,30 @@
 // importamos la funcion que vamos a testear
-import { createUserWithEmailAndPassword, verifiedEmail }
-  from '../src/lib/firebase/registerFir.js';
+import { verifiedEmail } from '../src/lib/firebase/registerFir.js';
+import { auth, createUserWithEmailAndPassword } from '../src/lib/firebase/metFirebase.js';
 
-describe('test para la funcion de login', () => {
+jest.mock('../src/lib/firebase/metFirebase.js', () => {
+  return { 
+    auth: jest.fn(() => ({ auth: 'test' })),
+
+    createUserWithEmailAndPassword: jest.fn ((auth, email, password) => {
+      return Promise.resolve({usar: "admin"})
+    })
+    
+  }
+})
+
+describe('test para registrarse', () => {
   const email = 'petblr@test.com';
-  const password = 'Petblrlomejor123';
+  const password = '093848583';
 
-  it('Should call signInWithEmailAndPassword', async () => {
-    await verifiedEmail(email, password);
-    expect(createUserWithEmailAndPassword).toHaveBeenCalled();
-  });
-});
+  it('debería llamar a createUserWithEmailPassword', async () => {
+    await verifiedEmail(email, password)
+    expect(createUserWithEmailAndPassword).toHaveBeenCalled()
+  })
+
+  it('debería llamar a createUserWithEmailPassword con auth, email y pasar los argumentos' , async () => {
+    createUserWithEmailAndPassword.mockClear();
+    await verifiedEmail(email, password)
+    expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(auth, email, password)
+  }) 
+})
